1e9429be538ab3700405674e3398bed04a7b8629
evabou12 <ebou@iespompeufabra.cat>
2026-06-01 09:02:50 +0200

    Create sketch.js

---
+++ minionenfadat/sketch.js
@@ -0,0 +1,359 @@
+
+function setup() {
+  createCanvas(800, 1200);
+}
+
+function draw() {
+strokeWeight (5)
+
+  beginShape();
+beginShape();//cos
+  fill(239, 199, 67)
+curveVertex(400.75, 193.6875);
+curveVertex(226.75, 230.6875);
+curveVertex(159.75, 302.6875);
+curveVertex(129.75, 434.6875);
+curveVertex(121.75, 577.6875);
+curveVertex(118.75, 653.6875);
+curveVertex(130.75, 745.6875);
+curveVertex(166.75, 850.6875);
+curveVertex(225.75, 883.6875);
+curveVertex(279.75, 898.6875);
+curveVertex(300.75, 919.6875);
+curveVertex(309.75, 938.6875);
+curveVertex(295.75, 954.1875);
+curveVertex(294.75, 982.1875);
+curveVertex(372.75, 984.1875);
+curveVertex(390.75, 976.1875);
+curveVertex(388.75, 934.1875);
+curveVertex(397.75, 900.1875);
+curveVertex(402.75, 931.1875);
+curveVertex(405.75, 972.1875);
+curveVertex(442.75, 987.1875);
+curveVertex(498.75, 987.1875);
+curveVertex(501.75, 960.1875);
+curveVertex(487.75, 951.1875);
+curveVertex(489.75, 928.1875);
+curveVertex(507.75, 906.1875);
+curveVertex(597.75, 871.6875);
+curveVertex(663.75, 785.6875);
+curveVertex(673.75, 691.6875);
+curveVertex(666.75, 458.6875);
+curveVertex(627.75, 286.6875);
+curveVertex(520.75, 218.6875);
+curveVertex(402.75, 193.6875);
+endShape(CLOSE)
+  
+
+  
+
+  
+   //ull esquerre
+  fill(198, 212, 215)
+ ellipse(280,350,255,255);//part exterior ç
+  fill(250, 245, 252)
+    ellipse(280,350,160,160);//part mitjana esquerre
+  fill(129, 116, 61)
+ ellipse(312,352,54,54);//pupila esquerre   
+  fill(10, 13, 18)
+      ellipse(312,353,27,27);// pupila esquerra petita
+  
+  //ull dret
+  fill(198, 212, 215)
+  ellipse(522,351,255,255);//part exterior dreta
+  fill(250, 245, 252)
+ellipse(522,351,160,160);//pupila mitjana dreta
+  fill(221, 135, 74)
+    ellipse(483,353,54,54)
+  fill(10, 13, 18)
+ ellipse(483,353,27,27);//pupili petita dreta
+
+  
+beginShape();//ROBA
+ fill (65, 118, 179)
+curveVertex(121.5, 546.609375);
+curveVertex(163.5, 567.609375);
+curveVertex(210.5, 603.609375);
+curveVertex(236.5, 624.609375);
+curveVertex(286.5, 623.609375);
+curveVertex(332.5, 625.609375);
+curveVertex(360.5, 606.609375);
+curveVertex(394.5, 605.609375);
+curveVertex(409.5, 616.609375);
+curveVertex(432.5, 597.609375);
+curveVertex(478.5, 601.609375);
+curveVertex(502.5, 625.609375);
+curveVertex(556.5, 622.609375);
+curveVertex(589.5, 597.609375);
+curveVertex(630.5, 565.609375);
+curveVertex(665.5, 543.609375);
+curveVertex(678.5, 541.609375);
+curveVertex(685.5, 582.609375);
+curveVertex(581.5, 661.609375);
+curveVertex(584.5, 753.609375);
+curveVertex(670.5, 744.609375);
+curveVertex(665.5, 791.609375);
+curveVertex(642.5, 827.609375);
+curveVertex(613.5, 856.609375);
+curveVertex(568.5, 883.609375);
+curveVertex(525.5, 897.609375);
+curveVertex(498.5, 915.609375);
+curveVertex(491.5, 935.609375);
+curveVertex(403.5, 935.609375);
+curveVertex(395.5, 894.609375);
+curveVertex(392.5, 932.609375);
+curveVertex(307.5, 934.609375);
+curveVertex(281.5, 902.609375);
+curveVertex(212.5, 877.609375);
+curveVertex(155.5, 834.609375);
+curveVertex(122.5, 753.609375);
+curveVertex(210.5, 755.609375);
+curveVertex(215.5, 666.609375);
+curveVertex(143.5, 605.609375);
+curveVertex(113.5, 591.609375);
+curveVertex(121.5, 545.609375);
+endShape(CLOSE);
+beginShape();//GUANT ESQUERRE
+fill (28, 32, 41)
+curveVertex(413.5, 618.609375);
+curveVertex(394.5, 606.609375);
+curveVertex(368.5, 605.609375);
+curveVertex(342.5, 617.609375);
+curveVertex(326.5, 644.609375);
+curveVertex(331.5, 674.609375);
+curveVertex(319.5, 657.609375);
+curveVertex(306.5, 668.609375);
+curveVertex(306.5, 689.609375);
+curveVertex(305.5, 722.609375);
+curveVertex(338.5, 721.609375);
+curveVertex(373.5, 701.609375);
+curveVertex(397.5, 681.609375);
+curveVertex(376.5, 651.609375);
+curveVertex(401.5, 671.609375);
+curveVertex(411.5, 656.609375);
+curveVertex(414.5, 617.609375);
+endShape(CLOSE);
+  
+  beginShape();//GUANT DRET
+  fill (28, 32, 41)
+curveVertex(415.5, 617.609375);
+curveVertex(430.5, 597.609375);
+curveVertex(465.5, 596.609375);
+curveVertex(495.5, 615.609375);
+curveVertex(508.5, 648.609375);
+curveVertex(495.5, 685.609375);
+curveVertex(513.5, 655.609375);
+curveVertex(524.5, 663.609375);
+curveVertex(524.5, 694.609375);
+curveVertex(512.5, 726.609375);
+curveVertex(480.5, 716.609375);
+curveVertex(451.5, 684.609375);
+curveVertex(468.5, 650.609375);
+curveVertex(442.5, 671.609375);
+curveVertex(425.5, 662.609375);
+curveVertex(414.5, 618.609375);
+endShape(CLOSE);
+  
+  beginShape();//BRAÇ ESQUERRE
+fill(239, 199, 67)
+curveVertex(157.5, 632.609375);
+curveVertex(181.5, 656.609375);
+curveVertex(227.5, 670.609375);
+curveVertex(303.5, 670.609375);
+curveVertex(300.5, 722.609375);
+curveVertex(217.5, 718.609375);
+curveVertex(156.5, 697.609375);
+curveVertex(117.5, 659.609375);
+endShape();
+  
+  beginShape();//BRAÇ DRET
+ fill(239, 199, 67)
+curveVertex(649.5, 629.609375);
+curveVertex(618.5, 654.609375);
+curveVertex(554.5, 669.609375);
+curveVertex(526.5, 667.609375);
+curveVertex(522.5, 716.609375);
+curveVertex(585.5, 714.609375);
+curveVertex(640.5, 698.609375);
+curveVertex(677.5, 663.609375);
+endShape();
+  
+ beginShape();//BUXACA gran
+ fill(74, 141, 184)
+vertex(321.5, 727.609375);
+vertex(329.5, 747.609375);
+vertex(341.5, 761.609375);
+vertex(364.5, 770.609375);
+vertex(397.5, 771.609375);
+vertex(434.5, 769.609375);
+vertex(459.5, 763.609375);
+vertex(468.5, 751.609375);
+vertex(473.5, 728.609375);
+vertex(474.5, 713.609375);
+vertex(461.5, 700.609375);
+vertex(449.5, 686.609375);
+vertex(428.5, 681.609375);
+vertex(413.5, 682.609375);
+vertex(395.5, 687.609375);
+vertex(380.5, 697.609375);
+vertex(365.5, 711.609375);
+vertex(350.5, 720.609375);
+vertex(322.5, 725.609375);
+endShape();
+
+beginShape();//BOCA
+  fill(255, 39, 18)
+ellipse(400,538, 85,85)
+endShape(CLOSE);
+  
+beginShape();//CORDA ULLERES ESQUERRE
+  fill(28, 32, 41)
+curveVertex(165.5, 387.609375);
+curveVertex(130.5, 398.609375);
+curveVertex(144.5, 332.609375);
+curveVertex(157.5, 329.609375);
+curveVertex(156.5, 358.609375);
+curveVertex(166.5, 384.609375);
+endShape(CLOSE);
+  
+  
+  beginShape();//CORDA ULLERES DRETA
+  fill(28, 32, 41)
+curveVertex(636.5, 307.609375);
+curveVertex(654.5, 342.609375);
+curveVertex(668.5, 401.609375);
+curveVertex(635.5, 387.609375);
+curveVertex(643.5, 357.609375);
+curveVertex(636.5, 308.609375);
+endShape(CLOSE);
+  
+  
+  beginShape();//sabata esquerre
+  fill(28, 32, 41)
+curveVertex(386.5, 939.609375);
+curveVertex(392.5, 976.609375);
+curveVertex(348.5, 993.609375);
+curveVertex(291.5, 989.609375);
+curveVertex(294.5, 961.609375);
+curveVertex(312.5, 944.609375);
+curveVertex(316.5, 936.609375);
+curveVertex(385.5, 939.609375);
+endShape(CLOSE);
+  
+beginShape();//sabata dreta
+curveVertex(408.5, 936.609375);
+curveVertex(404.5, 976.609375);
+curveVertex(443.5, 993.609375);
+curveVertex(497.5, 990.609375);
+curveVertex(502.5, 962.609375);
+curveVertex(485.5, 946.609375);
+curveVertex(482.5, 935.609375);
+curveVertex(407.5, 933.609375);
+endShape(CLOSE);
+
+beginShape(); //BUTXACA ESQUERRE
+   fill(74, 141, 184)
+curveVertex(211.5, 761.609375);
+curveVertex(207.5, 786.609375);
+curveVertex(188.5, 810.609375);
+curveVertex(165.5, 815.609375);
+curveVertex(145.5, 816.609375);
+curveVertex(132.5, 793.609375);
+curveVertex(123.5, 749.609375);
+curveVertex(208.5, 761.609375);
+endShape(CLOSE);
+  
+  
+  beginShape();//BUTXACA DRETA
+  fill(74, 141, 184)
+curveVertex(584.5, 762.609375);
+curveVertex(586.5, 781.609375);
+curveVertex(598.5, 807.609375);
+curveVertex(631.5, 817.609375);
+curveVertex(649.5, 813.609375);
+curveVertex(668.5, 782.609375);
+curveVertex(669.5, 749.609375);
+curveVertex(586.5, 761.609375);
+endShape(CLOSE);
+  
+//boto esquerre
+  fill(27, 38, 102)
+  ellipse(237,650,25,25)
+  
+  //boto dret
+    fill(27, 38, 102)
+   ellipse(560,650,25,25)
+  
+  beginShape();//part ulleres esquerre
+   fill(198, 212, 215)
+vertex(163.5, 306.609375);
+vertex(146.5, 306.609375);
+vertex(147.5, 399.609375);
+vertex(166.5, 402.609375);
+vertex(158.5, 373.609375);
+vertex(154.5, 340.609375);
+vertex(164.5, 308.609375);
+endShape();
+  
+  
+  beginShape();//part ulleres dreta
+   fill(198, 212, 215)
+vertex(635.5, 305.609375);
+vertex(649.5, 308.609375);
+vertex(647.5, 401.609375);
+vertex(630.5, 400.609375);
+vertex(638.5, 375.609375);
+vertex(638.5, 341.609375);
+vertex(633.5, 304.609375);
+endShape();
+  
+beginShape();// linea 1
+vertex(176.5, 243.609375);
+vertex(124.5, 192.609375);
+endShape();
+  
+  
+  beginShape();//linea 2
+vertex(211.5, 135.609375);
+vertex(244.5, 187.609375);
+endShape();
+  
+ 
+  beginShape();//linea 4
+vertex(389.5, 101.609375);
+vertex(390.5, 169.609375);
+endShape();
+  
+  beginShape();//linea 3
+vertex(297.5, 126.609375);
+vertex(324.5, 184.609375);
+endShape();
+  
+  beginShape();//linea 0
+vertex(32.5, 280.609375);
+vertex(120.5, 327.609375);
+endShape();
+  
+  beginShape();//linea 5
+vertex(514.5, 122.609375);
+vertex(491.5, 190.609375);
+endShape();
+  
+  beginShape();//linea 7
+vertex(650.5, 173.609375);
+vertex(596.5, 232.609375);
+endShape();
+  
+  beginShape();//linea 6
+vertex(598.5, 155.609375);
+vertex(553.5, 209.609375);
+endShape();
+  
+  beginShape();//linea 8
+vertex(733.5, 246.609375);
+vertex(663.5, 295.609375);
+endShape();
+  
+  
+  
+}
